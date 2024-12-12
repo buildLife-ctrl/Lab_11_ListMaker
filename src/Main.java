47d8a3741b69787165f675a9d2dd432cf7176aea
617998 <sinchuri.617998@swl.k12.oh.us>
2024-12-12 08:28:36 -0500

    2ini

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,15 @@
-import java.applet.Applet;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static java.lang.String.*;
+
 public class Main {
     static ArrayList<String> list = new ArrayList<String>(10);
+    static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
         boolean quit = false;
 
         System.out.print("List of fruits listed as one of each letter:");
-
-        do {
-            displayList();
-
-            System.out.println();
-            System.out.println("Options:");
-            System.out.println("A - Add an item to the list");
-            System.out.println("D – Delete an item from the list");
-            System.out.println("P – Print the list");
-            System.out.println("Q – Quit the program");
-            System.out.println();
-            String ask = getRegExString(scan, "Please pick one of the options above:", "[AaDdPpQq]");
-            if (ask.equalsIgnoreCase("a")) {
-                add(scan);
-            }
-
-            quit = quit(scan, "\nWould you like to quit? [Y/N]");
-        } while (!quit);
-    }
-
-    public static void displayList() { //10 fruits that I came up with as letters increment
         list.add("Apple");
         list.add("Banana");
         list.add("Carrot");
@@ -42,6 +21,31 @@ public class Main {
         list.add("Indian jujube");
         list.add("Jackfruit");
 
+        displayList(); //Just so it does not keep displaying everytime
+        do {
+            System.out.println(); //makes it look better
+            System.out.println("Options:");
+            System.out.println("A - Add an item to the list");
+            System.out.println("D – Delete an item from the list");
+            System.out.println("P – Print the list");
+            System.out.println("Q – Quit the program");
+            System.out.println(); //for better structure
+            String ask = getRegExString(scan, "Please pick one of the options above:", "[AaDdPpQq]");
+
+            System.out.println(); //for better structure
+            if (ask.equalsIgnoreCase("a")) {
+                add(scan);
+            } else if (ask.equalsIgnoreCase("d")) {
+                delete(scan);
+            } else if (ask.equalsIgnoreCase("p")) {
+                print();
+            } else {
+                quit = quit(scan, "Are you sure you want to quit? [Y/N]");
+            }
+        } while (!quit);
+    }
+
+    public static void displayList() { //10 fruits that I came up with as letters increment
         for (int i = 0; i < list.size(); i++) {
             System.out.printf("\n%-2s %3s", i + 1, " - ");
             System.out.printf(list.get(i));
@@ -49,11 +53,32 @@ public class Main {
         System.out.println();
     }
 
-    public static String add(Scanner scan) {
-        String userInput = getNonZeroLenString(scan, "What would you like to add?");
+    public static void add(Scanner scan) {
+        String userInput = getNonZeroLenString(scan, "What would you like to add? PLease add a fruit starting" +
+                " with the next letter up from the list.");
+        userInput = Character.toString(userInput.charAt(0)).toUpperCase() + userInput.substring(1); //Alex helped
+        //the one underneath, I came up with it all by myself with programming I know
+        /*String i = valueOf(userInput.indexOf(0));
+        userInput = i.toUpperCase() + userInput.substring(1);*/
         list.add(userInput);
         displayList();
-        return userInput;
+    }
+
+    public static void delete(Scanner scan) {
+        int userInput = getRangedInt(scan, "Which array list value would you like to remove?" +
+                "[0-" + (list.size() - 1) + "]", 0, list.size());
+        list.remove(userInput);
+        displayList();
+    }
+
+    public static void print() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("%-2s %3s", i, " - ");
+            System.out.printf(list.get(i));
+            System.out.println();
+        }
+
+        System.out.println("\nIt is printed the way it is valued in the array list!");
     }
 
     public static String getNonZeroLenString(Scanner scan, String prompt) {
@@ -112,5 +137,27 @@ public class Main {
         } while (!check);
 
         return input;
+    }
+
+    public static int getRangedInt(Scanner scan, String prompt, int min, int max) {
+        int num = 0;
+        boolean check = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                num = scan.nextInt();
+                if (num >= min && num <= max) {
+                    check = true;
+                } else {
+                    System.out.println("Input not within the range, please try again.");
+                }
+            } else {
+                System.out.println("Invalid input, please try again.");
+            }
+            scan.nextLine();
+        } while (!check);
+
+        return num;
     }
 }
